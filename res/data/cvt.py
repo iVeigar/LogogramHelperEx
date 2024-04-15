ea3a6e54c165b43a219f852d9261ac7b0493c2cc
iVeigar <lyuwenchao8102@gmail.com>
2024-04-15 20:31:49 +0800

    Add submodule

--- res/data/cvt.py
+++
@@ -1,9 +0,0 @@
-import json
-a = []
-with open("logosActions.json", "r") as f:
-    data = json.load(f)
-    for action in data:
-        #print(f'{{ {logo["Id"]}, new(){{ Id = {logo["Id"]}, Name = "{logo["Name"]}" }} }},')
-        a.append(action["Recipes"])
-
-print(a)
